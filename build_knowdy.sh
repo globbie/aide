cf73f6a1b647f6a621badfe25209640e27e58896
Ilia K <ki.stfu@gmail.com>
2018-11-11 00:19:00 +0300

    Add build_knowdy.sh script

---
+++ build_knowdy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+ROOT_DIR="$(dirname $(readlink -f "$0"))"
+
+KNOWDY_SRC="${ROOT_DIR}/pkg/knowdy/knowdy"
+KNOWDY_BUILD="${KNOWDY_SRC}/build"
+
+[ -d "${KNOWDY_SRC}" ] || (echo "knowdy not found" && exit 1)
+
+if [ ! -d "${KNOWDY_BUILD}" ]; then
+    mkdir "${KNOWDY_BUILD}" && cd "${KNOWDY_BUILD}"
+    cmake "${KNOWDY_SRC}" -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=`go env CC`
+    cd -
+fi
+
+make -C "${KNOWDY_BUILD}" -j knowdy_static
+
