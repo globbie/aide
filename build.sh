d1f82353c2fe3daf5d97476ca7794783876209c8
Dmitri D <dmitrid@MacBook-Pro-MacBook.local>
2019-09-27 11:24:24 +0300

    enable multi-stage docker build

--- build.sh
+++ build.sh
@@ -2,18 +2,4 @@
 
 set -e
 
-docker build -t tmp -f Dockerfile.build .
-
-id=$(docker create tmp)
-
-docker cp ${id}:/tmp/gnode .
-docker cp ${id}:/tmp/schemas .
-docker cp ${id}:/tmp/coverage.out .
-
-docker rm -v ${id}
-docker rmi tmp
-
 docker build -t globbie/gnode:$TAG .
-
-#rm gnode
-#rm -rf schemas
